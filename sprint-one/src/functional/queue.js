c8999bbc7c498865a84c7723bdaac14fab0d825c
nassir <nassirmz@gmail.com>
2016-03-03 16:18:24 -0800

    Add comments for functional section

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,19 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  // Two indexes, one to add to the back, one to take from front.
   var backIndex = 0;
   var frontIndex = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    //add value to back of storage
     storage[backIndex] = value;
     backIndex++;
   };
 
   someInstance.dequeue = function(){
+    //remove first elements from storage and return it
     var temp = storage[frontIndex];
     delete storage[frontIndex];
     frontIndex++;
@@ -20,6 +23,7 @@ var Queue = function(){
   };
 
   someInstance.size = function(){
+    //compare index sizes to get current number of elements in storage
     if (backIndex - frontIndex >= 0) {
       return backIndex - frontIndex;
     }  else {
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,5 +1,6 @@
 var Stack = function(){
   var someInstance = {};
+  //keep track of size
   var count = 0;
 
   // Use an object with numeric keys to store values
@@ -7,20 +8,24 @@ var Stack = function(){
 
   // Implement the methods below
   someInstance.push = function(value){
+    //add value to back of storage
     storage[count] = value;
     count++;
   };
 
   someInstance.pop = function(){
+    //make sure pop doesn't work on empty storage
     if(count >0) {
       count--;
     }
+    //delete last element from stack and return it
     var temp = storage[count];
     delete storage[count];
     return temp;
   };
 
   someInstance.size = function(){
+    //return total count
     return count;
   };
 
