88734492bbc03d60817f59405f599f2dccb0a0ef
nassir <nassirmz@gmail.com>
2016-03-04 10:40:47 -0800

    Complete LinkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,13 +3,44 @@ var LinkedList = function(){
   list.head = null;
   list.tail = null;
 
+  //O(1)
   list.addToTail = function(value){
+    //assign first node as head/tail once
+    if(!list.head) {
+      list.head = Node(value);
+      list.tail = list.head;
+      //assign next node as head.next and tail once
+    } else if (list.head === list.tail){
+      list.head.next = Node(value);
+      list.tail = Node(value);
+      //adds the rest to the linked list
+    } else {
+      var temp = Node(value);
+      list.tail.next = temp;
+      list.tail = temp;
+    }
   };
 
+  //O(1)
   list.removeHead = function(){
+    //use temp to hold old head and return its value
+    var temp = list.head;
+    //set new head to next list in line
+    list.head = temp.next;
+    return temp.value;
   };
 
+  //O(n)
   list.contains = function(target){
+    //start at head, iterate to the end
+    var temp = list.head;
+    while(temp) {
+      if(temp.value ===target) {
+        return true;
+      }
+      temp = temp.next;
+    }
+    return false;
   };
 
   return list;
