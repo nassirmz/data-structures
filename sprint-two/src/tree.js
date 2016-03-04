d56d242cd837569ec91fcc00a04d9f8189b163a1
nassir <nassirmz@gmail.com>
2016-03-04 11:42:25 -0800

    complete Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,25 +1,31 @@
-var Tree = function(value){
+  var Tree = function(value){
   var newTree = {};
   newTree.value = value;
-
+  _.extend(newTree, treeMethods);
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
 
-
-
-
-
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target){
-
+  var result = false;
+  function walkTree(tree) {
+    if(tree.value === target) {
+      result = true;
+    }
+    for(var i = 0; i < tree.children.length; i +=1) {
+      walkTree(tree.children[i]);
+    }
+    return result;
+  }
+  return walkTree(this);
 };
 
 
