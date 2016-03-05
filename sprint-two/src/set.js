2154f977d5b8e91b2c196cb205880b42e5d7ff14
nassir <nassirmz@gmail.com>
2016-03-04 19:39:32 -0800

    Complete set.js

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,12 +7,22 @@ var Set = function(){
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  if (!this.contains(item))
+  this[item] = item;
 };
 
 setPrototype.contains = function(item){
+  for (var key in this) {
+    if (this[key] === item) {
+      return true;
+    }
+  }
+  return false;
+
 };
 
 setPrototype.remove = function(item){
+  delete this[item];
 };
 
 /*
