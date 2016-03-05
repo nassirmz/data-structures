29764b42024d0b5e5e1a7ff8b064b2120ca3216f
nassir <nassirmz@gmail.com>
2016-03-05 09:49:40 -0800

    Complete hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,48 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  //set bucket if storage already exists
+  var bucket = this._storage.get(i);
+  var contains = false;
+  //create bucket if no storage exists
+  if (!bucket) {
+    bucket = [];
+    bucket.push([k,v]);
+  } else {
+    //checks for existing keys value pairs
+    for (var i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === k) {
+        bucket[i][1] = v;
+        contains = true;
+      } else if (!contains) {
+        bucket.push([k,v]);
+      }
+    }
+  }
+  //set bucket to storage i
+  this._storage.set(i, bucket);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  //go through bucket and make sure key matches
+  var bucket = this._storage.get(i);
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      return bucket[i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  //go through bucket, remove correct key value pair.
+  var bucket = this._storage.get(i);
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      bucket[i][1] = null;
+    }
+  }
 };
 
 
